5fdd0f7fa551f6cd50cd18bde0ab52312f65ba8a
Min Jun <qkralswns931026@gmail.com>
2023-08-30 14:37:12 +0900

    [DEV] 개발 환경 구성

---
+++ ObjectPoolSystem/Assets/Utility/Scripts/ObjectPool.cs
@@ -0,0 +1,16 @@
+// ----- C#
+using System.Collections;
+using System.Collections.Generic;
+
+// ----- Unity
+using UnityEngine;
+
+namespace Utility.ForObjectPool
+{
+    public class ObjectPool<TKey> : MonoBehaviour
+    {
+        // --------------------------------------------------
+        // Components
+        // --------------------------------------------------
+    }
+}
