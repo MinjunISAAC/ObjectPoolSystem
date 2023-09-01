6d2e0932ecb46ce8787097cf7bbc16877aec6787
Min Jun <qkralswns931026@gmail.com>
2023-09-01 13:54:58 +0900

    [DEV] Object Pool Utility 폴더 구조 수정

---
+++ ObjectPoolSystem/Assets/Utility/ObjectPoolSystem/Scripts/ObjectPool.cs
@@ -0,0 +1,87 @@
+// ----- C#
+using System.Collections;
+using System.Collections.Generic;
+
+// ----- Unity
+using UnityEngine;
+
+namespace Utility.ForObjectPool
+{
+    [System.Serializable]
+    public sealed class ObjectPool<TKey> : MonoBehaviour where TKey : Component
+    {
+        // --------------------------------------------------
+        // Constructor
+        // --------------------------------------------------
+        public ObjectPool(int capacity) { this._capacity = capacity; }
+
+        // --------------------------------------------------
+        // Components
+        // --------------------------------------------------
+        private TKey        _targetObject = default;
+        private int         _objectIndex  = -1;
+        private Queue<TKey> _pool         = default;
+        private Transform   _parents      = null;
+
+        // --------------------------------------------------
+        // Variables
+        // --------------------------------------------------
+        private int _capacity = 0;
+
+        // --------------------------------------------------
+        // Properties
+        // --------------------------------------------------
+        public TKey OriginObject => _targetObject;
+
+        // --------------------------------------------------
+        // Functions - Nomal
+        // --------------------------------------------------
+        // ----- Public
+        public void OnInit(TKey targetObject, Transform parents)
+        {
+            if (_targetObject != null)
+                return;
+            
+            _targetObject = targetObject;
+            _parents      = parents;
+
+            for (int i = 0; i < _capacity; i++) _pool.Enqueue(_CreatedObejct());
+        }
+
+        public TKey GetObject(Transform parents = null)
+        {
+            TKey obj = default;
+
+            if (_pool.Count > 0) 
+            {
+                obj = _pool.Dequeue();
+            }  
+            else
+            {
+                obj = _CreatedObejct();
+                _pool.Enqueue(obj);
+            }
+
+            obj.transform.SetParent(parents);
+            obj.gameObject.SetActive(true);
+
+            return obj;
+        }
+
+        public void ReturnObject(TKey obj)
+        {
+            obj.gameObject.SetActive(false);
+            obj.transform.SetParent(_parents);
+            _pool.Enqueue(obj);
+        }
+
+        // ----- Private
+        private TKey _CreatedObejct()
+        {
+            TKey obj = Instantiate(_targetObject, _parents).GetComponent<TKey>();
+            obj.gameObject.SetActive(false);
+            return obj;
+        }
+
+    }
+}
