6dff533ead884e8db655836ebd1a2586622bde2d
Min Jun <qkralswns931026@gmail.com>
2023-09-01 12:57:35 +0900

    [DEV] Npc 추가

---
+++ ObjectPoolSystem/Assets/TestGame/Scripts/Npc/Npc.cs
@@ -0,0 +1,14 @@
+// ----- C#
+using System.Collections;
+using System.Collections.Generic;
+
+// ----- Unity
+using UnityEngine;
+
+namespace InGame.ForUnit
+{
+    public class Npc : MonoBehaviour
+    {
+    
+    }
+}
---
+++ ObjectPoolSystem/Assets/TestGame/Scripts/Npc/NpcController.cs
@@ -0,0 +1,17 @@
+// ----- C#
+using System.Collections;
+using System.Collections.Generic;
+
+// ----- Unity
+using UnityEngine;
+
+namespace InGame.ForNpc.Manage
+{
+    public class NpcController : MonoBehaviour
+    {
+        // --------------------------------------------------
+        // Components
+        // --------------------------------------------------
+
+    }
+}
